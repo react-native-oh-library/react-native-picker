ce8e1e3d2fd43f44c0fc7f1a2da131b0e5b57e57
maka <113342266+ajkshfjkaj@users.noreply.github.com>
2024-10-19 09:44:35 +0800

    fix: 修复引入时出现的错误 (#13)
    Co-authored-by: maikai (A) <mWX1352063@china.huawei.com>

--- harmony/picker/ts.ts
+++ harmony/picker/ts.ts
@@ -1,3 +1,3 @@
-// export * from "./src/main/ets/PickerPackage";
-//
-// export * from "./src/main/ets/PickerTurboModule";
+export * from "./src/main/ets/PickerPackage";
+
+export * from "./src/main/ets/PickerTurboModule";
