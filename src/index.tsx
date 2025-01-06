3f618fc2d7ce115af9447ab152203e82c94d7a16
mWX1352063 <2742191065@qq.com>
2025-01-06 14:22:20 +0800

    fix: 修复初始选择没有索引问题

--- src/index.tsx
+++ src/index.tsx
@@ -35,7 +35,7 @@ export default {
         //there are no `removeListener` for NativeAppEventEmitter & DeviceEventEmitter
         this.listener && this.listener.remove();
         this.listener = DeviceEventEmitter.addListener('pickerEvent', event => {
-            fnConf[event['type']](event['selectedValue']);
+            fnConf[event['type']](event['selectedValue'],event['selectedIndex']);
         });
     },
 
